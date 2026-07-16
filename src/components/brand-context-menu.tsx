d8cc0f9fe0fc05726ffe9bcd7aa559aa092f4688
ijquenta <ivanjosuequentavargas@gmail.com>
2026-07-15 22:46:50 -0400

    refactor: simplify BrandContextMenu by removing unused items and updating link

--- src/components/brand-context-menu.tsx
+++ src/components/brand-context-menu.tsx
@@ -1,67 +1,27 @@
 "use client"
 
 import Link from "next/link"
-import { copyText } from "@/utils/copy"
-import { useTiks } from "@rexa-developer/tiks/react"
-import { DownloadIcon } from "@animateicons/react/lucide"
-import { SquareDashed, Type } from "lucide-react"
-import { toast } from "sonner"
 
 import {
   ContextMenu,
   ContextMenuContent,
   ContextMenuItem,
-  ContextMenuSeparator,
   ContextMenuTrigger,
 } from "@/components/ui/context-menu"
 
-import { ChanhDaiMark, getMarkSVG } from "./chanhdai-mark"
-import { getWordmarkSVG } from "./chanhdai-wordmark"
+import { SiteMark } from "./site-mark"
 
 export function BrandContextMenu({ children }: { children: React.ReactNode }) {
-  const { success } = useTiks()
-
   return (
     <ContextMenu>
       <ContextMenuTrigger>{children}</ContextMenuTrigger>
 
       <ContextMenuContent className="w-fit">
-        <ContextMenuItem
-          onClick={() => {
-            copyText(getMarkSVG())
-            toast.success("Mark as SVG copied")
-            success()
-          }}
-        >
-          <ChanhDaiMark />
-          Copy Mark as SVG
-        </ContextMenuItem>
-
-        <ContextMenuItem
-          onClick={() => {
-            copyText(getWordmarkSVG())
-            toast.success("Logotype as SVG copied")
-            success()
-          }}
-        >
-          <Type />
-          Copy Logotype as SVG
-        </ContextMenuItem>
-
-        <ContextMenuSeparator />
-
         <ContextMenuItem asChild>
-          <Link href="/blog/chanhdai-brand">
-            <SquareDashed />
-            Brand Guidelines
+          <Link href="/" className="flex items-center gap-2">
+            <SiteMark className="size-5" />
+            ijquenta.dev
           </Link>
-        </ContextMenuItem>
-
-        <ContextMenuItem asChild>
-          <a href="https://assets.chanhdai.com/chanhdai-brand.zip" download>
-            <DownloadIcon />
-            Download Brand Assets
-          </a>
         </ContextMenuItem>
       </ContextMenuContent>
     </ContextMenu>
