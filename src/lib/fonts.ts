063ec654eec5e191a03cec185e0c8a186e5d9a98
ijquenta <ivanjosuequentavargas@gmail.com>
2026-07-18 23:58:46 -0400

    feat: remove unused pixelatedMSSansSerif font and commented-out code to streamline codebase

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -15,34 +15,11 @@ const fontSerif = localFont({
   variable: "--font-serif",
 })
 
-// const fontPixel = localFont({
-//   src: "../assets/fonts/DepartureMono-Regular.woff2",
-//   weight: "400",
-//   fallback: ["monospace"],
-//   variable: "--font-pixel",
-// })
-
-const pixelatedMSSansSerif = localFont({
-  src: [
-    {
-      path: "../assets/fonts/ms_sans_serif.woff2",
-      weight: "400",
-    },
-    {
-      path: "../assets/fonts/ms_sans_serif_bold.woff2",
-      weight: "700",
-    },
-  ],
-  fallback: ["Arial"],
-  variable: "--font-98cn",
-})
-
 export const fontVariables = cn(
   fontSans.variable,
   fontMono.variable,
   fontSerif.variable,
   GeistPixelSquare.variable,
-  pixelatedMSSansSerif.variable,
   "[--font-sans:var(--font-geist-sans)]",
   "[--font-mono:var(--font-geist-mono)]"
 )
